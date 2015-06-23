b1d193b43ec4f922f8c293cdff4296cafb92e994
Kingston Duffie <kduffie@sevogle.com>
2015-06-22 20:02:49 -0700

    Implementation of tile mutation processing in bot -- not tested

--- braid-bot.js
+++ braid-bot.js
@@ -3,7 +3,15 @@ var config;
 var messageSwitch = require('./braid-message-switch');
 var BraidAddress = require('./braid-address').BraidAddress;
 var getUserRecord = require('./braid-auth').getUserRecord;
+var TileMutationProcessor = require('./braid-tile-mutation-processor');
+var TileMutationMongoHandler = require('./braid-tile-mutation-mongo-handler');
+
+var braidDb;
 var BOT_RESOURCE = '!bot';
+
+var handler;
+
+var mutationProcessorsByTileId = {};
 
 function sendMessage(message) {
 	messageSwitch.deliver(message);
@@ -16,6 +24,97 @@ function createProxyAddress(userId) {
 function handlePing(message, to) {
 	var reply = factory.newReply(message, createProxyAddress(to.userId));
 	sendMessage(reply);
+}
+
+function handleTileShare(message) {
+	// When we get a tile-share, it is either from our own user, or from
+	// someone else. If the former, we'll accept the tile unless we already
+	// have it. If the latter, we'll ignore it, and will eventually add it
+	// to a list of pending shares that our user can use later.
+
+	// If the share is not from our domain, then we won't be accepting, because
+	// it can have been shared by one of our users.
+
+	if (message.from.domain !== config.domain) {
+		return;
+	}
+
+	// The tile share could have been sent to multiple addresses. We only
+	// need to accept it once regardless, because when we accept from the
+	// same identity, it won't cause anyone to be added as a member.
+
+	boolean
+	ownShare = false;
+
+	for (var i = 0; i < message.to.length; i++) {
+		var to = message.to[i];
+		if (message.from.userId === to.userId && to.domain === config.domain) {
+			ownShare = true;
+			break;
+		}
+	}
+
+	if (!ownShare) {
+		return;
+	}
+
+	if (message.data && message.data.tileId) {
+		braidDb.findTileById(message.data.tileId, function(err, tileRecord) {
+			if (err) {
+				console.error("Failure finding tile in db", err);
+			} else if (!record) {
+				// We don't yet have this tile. So we'll save the tile and
+				// issue a tile-accept to get them to send us the mutations for it.
+				var record = factory.newTileRecordFromInfo(message.data);
+				braidDb.insertUserTile(record, function(err) {
+					if (err) {
+						console.error("Failure inserting tile", err);
+					} else {
+						var acceptMessage = factory.newTileAcceptMessage(message.from, createProxyAddress(message.from.userId), message.data.tileId);
+						sendMessage(acceptMessage);
+					}
+				});
+			}
+		});
+	}
+}
+
+function processMutation(tileRecord, mutation) {
+	process.nextTick(function() {
+		// Find or create a tile mutation processor for the tile
+		var mp = mutationProcessorsByTileId[mutation.tileId];
+		if (!mp) {
+			mp = new TileMutationProcessor(mutation.tileId, tileRecord.mutationCount, tileMutationHandlers);
+			mutationProcessorsByTileId[mutation.tileId] = mp;
+		}
+		mp.addMutation(mutation);
+	});
+}
+
+function handleTileMutation(message, to) {
+	// If we receive a tile mutation, we will process it, but only if it is
+	// for a tile that we have. In that case, we don't care who they were
+	// sending it to.
+
+	if (message.data && message.data.tileId) {
+		braidDb.findTileById(message.data.tileId, function(err, tileRecord) {
+			if (err) {
+				console.error("Failure finding tile in db", err);
+			} else if (tileRecord) {
+				// Now we check to see if we already have this mutation and it
+				// has been processed.
+				braidDb.findMutation(message.data.tileId, message.data.mutationId, function(err, mutationRecord) {
+					if (err) {
+						console.err("Failure finding mutation", err);
+					} else if (!record || !record.integrated) {
+						console.log("braid-bot:  Received mutation to be integrated", message.data);
+						processMutation(tileRecord, message.data);
+					}
+				});
+			}
+		});
+	}
+
 }
 
 function handleMessage(message, to, isDirected) {
@@ -34,6 +133,10 @@ function handleMessage(message, to, isDirected) {
 			case 'cast':
 				switch (message.request) {
 				case 'tile-share':
+					handleTileShare(message, to);
+					break;
+				case 'tile-mutation':
+					handleTileMutation(message, to);
 					break;
 				}
 				break;
@@ -86,10 +189,23 @@ function messageHandler(message) {
 
 }
 
-function initialize(configuration) {
+function handleOnFileMissing(tileId, mutation) {
+	// TODO: request file from originator
+}
+
+function handleOnMutationsCompleted(tileId) {
+	delete mutationProcessorsByTileId[tileId];
+}
+
+function initialize(configuration, db) {
 	console.log("braid-client-bot: initializing");
 	config = configuration;
+	braidDb = db;
 	messageSwitch.registerHook(messageHandler);
+	mutationHandler = new TileMutationMongoHandler({
+		onFileMissing : handleOnFileMissing,
+		onMutationsCompleted : handleOnMutationsCompleted,
+	}, braidDb);
 
 }
 module.exports = {
